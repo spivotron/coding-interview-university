654d178c542999234ecaf51c9f54e58938567639
Henry Spivey <spivotron@gmail.com>
2018-07-12 22:08:24 -0700

    working on excersizes.  stopped at 1-10

--- c programs/chap1/chap1/arrays.c
+++ c programs/chap1/chap1/arrays.c
@@ -7,3 +7,47 @@
 //
 
 #include <stdio.h>
+
+
+// count the number of occurrences of each digit
+//// blank, tab, newline, etc
+//int main(int argc, const char * argv[]){
+//    int c, i, nwhite, nother;
+//    int ndigit[10];
+//    nwhite = nother = 0;
+//    for (i = 0; i < 10; ++i) {
+//        ndigit[i] = 0;
+//    }
+//    
+//    while ( (c = getchar()) != EOF ) {
+//        if( c >= '0' && c <= '9' ) {
+//            ++ndigit[c-'0'];
+//        }
+//        else if(c == ' ' || c == '\n' || c == '\t') {
+//            ++nwhite;
+//        }
+//        else {
+//            ++nother;
+//        }
+//    }
+//    printf("digits = ");
+//    for (i = 0; i < 10; ++i) {
+//        printf(" %d", ndigit[i]);
+//    }
+//    printf(", white space = %d, other = %d\n", nwhite, nother);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- c programs/chap1/chap1/functions.c
+++ c programs/chap1/chap1/functions.c
@@ -9,12 +9,20 @@
 #include <stdio.h>
 
 int power(int m, int n);
+void convertMe(float);
+void lineCounter(void);
+void i2o(void);
+void i2o_v2(void);
 
 int main() {
-    int i;
-    for (i = 0; i< 10; ++i) {
-        printf("%d %d %d\n", i, power(2,i), power(-3, i));
-    }
+//    int i;
+//    for (i = 0; i< 10; ++i) {
+//        printf("%d %d %d\n", i, power(2,i), power(-3, i));
+//    }
+//    convertMe(-10.0);
+//    lineCounter();
+//    i2o();
+    i2o_v2();
     return 0;
 }
 
@@ -25,4 +33,67 @@ int power(int base, int n) {
         p = p * base;
     }
     return p;
+}
+
+// power function with call by value
+int power2(int base, int n) {
+    int p;
+    for (p=1; n>0; --n) {
+        p *= base;
+    }
+    return p;
+}
+
+// line counting
+void lineCounter() {
+    int c2, nl, nt;
+    nl = nt = 0;
+    while( (c2 = getchar()) != EOF ) {
+        if(c2== '\n') {
+            ++nl;
+        }
+        else if (c2 == '\t') {
+            ++nt;
+        }
+    }
+    printf("%d\n", nl);
+    printf("%d\n", nt);
+}
+
+//Exercise 1-9. Write a program to copy its input to its output, replacing each string of one or more blanks by a single blank
+void i2o() {
+    int c;
+    while ( (c = getchar()) != EOF) {
+        if ( c == ' ') {
+            while( (c = getchar()) == ' ' );
+            putchar(' ');
+        }
+        putchar(c);
+    }
+}
+
+void i2o_v2() {
+    int c;
+    while( (c =getchar()) != EOF ) {
+        if(c == '\t') {
+            while ( (c=getchar()) == '\t' );
+            putchar('\t');
+        }
+        else if(c == '\b') {
+            while ( (c=getchar()) == '\b' );
+            putchar('\b');
+        }
+        else if (c  == '\\') {
+            while ( (c=getchar()) == '\\' );
+            putchar('\\');
+        }
+        putchar(c);
+    }
+}
+
+
+void convertMe(float c) {
+    printf("Enter numbers in Celsius to convert to Fahrenheit. Enter STOP to end program \n");
+    float f = (9.0/5.0 *c) + 32;
+    printf("%.1f Celsius in Fahrenheit is %.1f \n", c, f);
 }
