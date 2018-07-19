a4212ade3e4cf0253f33f0a707c26930fa5970eb
Henry Spivey <spivotron@gmail.com>
2018-07-18 21:06:58 -0700

    stopped at 3.3.3 in python book

---
+++ c programs/chap2/chap2/chap2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  chap2
+//
+//  Created by Henry Spivey on 16/7/18.
+//  Copyright © 2018 Henry Spivey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
