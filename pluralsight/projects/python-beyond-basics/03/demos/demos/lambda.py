2be1c34e00f0c6a00a10c5ec3db36a9e0e50f50e
Henry Spivey <spivotron@gmail.com>
2018-09-27 07:10:35 -0700

    working on sorts

--- pluralsight/projects/python-beyond-basics/03/demos/demos/lambda.py
+++
@@ -1,4 +0,0 @@
-names = ["henry spivey", "dk bhardwaj", "jatinder verma"]
-print(names)
-newNames = sorted(names, key=lambda name:name.split()[-1]) # O(n log n)
-print(newNames)
--- pluralsight/projects/python-beyond-basics/03/demos/demos/sequence.py
+++
@@ -1,8 +0,0 @@
-def sequence_calss(immutable):
-    if immutable:
-        cls = tuple
-    else:
-        cls = list
-    return cls
-
-seq = sequence_calss(immutable=True)
