c7cc630670517633e673272890cfe2c89b39d88e
Henry Spivey <spivotron@gmail.com>
2018-10-16 07:43:35 -0700

    stopped at comparison operators

---
+++ phpjsmysql/phpnotes.php
@@ -0,0 +1,11 @@
+<?
+  echo "a: [". TRUE . "]";
+  echo "b: [". FALSE . "]";
+
+
+?>
+
+functions that check each type like is_array, is_object or is_bool
+
+A rarely used identity operator, which consists of three equals signs in a row, can be used to compare items without doing conversion.
+-------------- stopped at comparison operators ----------------
